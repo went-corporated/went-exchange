1ef1e4306cb897b19351c3849626b8993f7e0917
eKonnect <ekonn63@outlook.fr>
2021-08-07 14:52:01 +0200

    Diapositive system

---
+++ script.js
@@ -0,0 +1,24 @@
+let diapo1 = document.getElementById('diapositive_1');
+let diapo2 = document.getElementById('diapositive_2');
+let cornerBtn = document.getElementById('corner_btn');
+let cornerBtnArrow = document.getElementById('corner_btn_arrow');
+
+let clicked = false;
+let angle = 0;
+let transitionTime = .5;
+cornerBtn.onclick = function(){
+    if (clicked === false){
+        diapo1.style = `transform: translateX(-100%); transition: ${transitionTime}s;`;
+        diapo2.style = `transform: translateX(0%); transition: ${transitionTime}s;`;
+        cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
+        angle -= 180;
+        clicked = true;
+    }
+    else{
+        diapo1.style = `transform: translateX(0%); transition: ${transitionTime}s;`;
+        diapo2.style = `transform: translateX(100%); transition: ${transitionTime}s;`;
+        cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
+        angle -= 180;
+        clicked = false;
+    }
+}
