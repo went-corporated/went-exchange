19e274f606806b3312e8bcd40f9be81a963bd226
eKonnect <ekonn63@outlook.fr>
2021-08-09 17:34:28 +0200

    Media screen

--- script.js
+++ script.js
@@ -2,23 +2,36 @@ let diapo1 = document.getElementById('diapositive_1');
 let diapo2 = document.getElementById('diapositive_2');
 let cornerBtn = document.getElementById('corner_btn');
 let cornerBtnArrow = document.getElementById('corner_btn_arrow');
+let menubar = document.getElementById('menubar');
+let menubarToggleBtn = document.getElementById('menubar_toggle_btn');
 
 let clicked = false;
 let angle = 0;
 let transitionTime = .5;
-cornerBtn.onclick = function(){
-    if (clicked === false){
-        diapo1.style = `transform: translateX(-125%); transition: ${transitionTime}s;`;
-        diapo2.style = `transform: translateX(0%); transition: ${transitionTime}s;`;
-        cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
-        angle -= 180;
-        clicked = true;
-    }
-    else{
-        diapo1.style = `transform: translateX(0%); transition: ${transitionTime}s;`;
-        diapo2.style = `transform: translateX(125%); transition: ${transitionTime}s;`;
-        cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
-        angle -= 180;
-        clicked = false;
+// cornerBtn.onclick = function(){
+//     if (clicked === false){
+//         diapo1.style = `transform: translateX(-125%); transition: ${transitionTime}s;`;
+//         diapo2.style = `transform: translateX(0%); transition: ${transitionTime}s;`;
+//         cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
+//         angle -= 180;
+//         clicked = true;
+//     }
+//     else{
+//         diapo1.style = `transform: translateX(0%); transition: ${transitionTime}s;`;
+//         diapo2.style = `transform: translateX(125%); transition: ${transitionTime}s;`;
+//         cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
+//         angle -= 180;
+//         clicked = false;
+//     }
+// }
+
+let menubarToggleBtnClicked = false;
+menubarToggleBtn.onclick = function(){
+    if (menubarToggleBtnClicked === false){
+        menubar.style = 'transform: translateX(0px); transition: .3s;';
+        menubarToggleBtnClicked = true;
+    } else {
+        menubar.style = 'transform: translateX(-250px); transition: .3s;';
+        menubarToggleBtnClicked = false;
     }
 }
