88299b9a75d4bdf07b4a0f6122292cee315d05cd
eKonnect <ekonn63@outlook.fr>
2021-10-14 21:48:37 +0200

    (s. c.)

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ let menubarToggleBtn = document.getElementById('menubar_toggle_btn');
 let clicked = false;
 let angle = 0;
 let transitionTime = .5;
-// cornerBtn.onclick = function(){
-//     if (clicked === false){
-//         diapo1.style = `transform: translateX(-125%); transition: ${transitionTime}s;`;
-//         diapo2.style = `transform: translateX(0%); transition: ${transitionTime}s;`;
-//         cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
-//         angle -= 180;
-//         clicked = true;
-//     }
-//     else{
-//         diapo1.style = `transform: translateX(0%); transition: ${transitionTime}s;`;
-//         diapo2.style = `transform: translateX(125%); transition: ${transitionTime}s;`;
-//         cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
-//         angle -= 180;
-//         clicked = false;
-//     }
-// }
+cornerBtn.onclick = function(){
+    if (clicked === false){
+        diapo1.style = `transform: translateX(-125%); transition: ${transitionTime}s; opacity: 0;`;
+        diapo2.style = `transform: translateX(0%); transition: ${transitionTime}s; opacity: 1;`;
+        cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
+        angle -= 180;
+        clicked = true;
+    }
+    else{
+        diapo1.style = `transform: translateX(0%); transition: ${transitionTime}s; opacity: 1;`;
+        diapo2.style = `transform: translateX(125%); transition: ${transitionTime}s; opacity: 0;`;
+        cornerBtnArrow.style = `transform: rotate(${angle}deg); transition: ${transitionTime}s;`;
+        angle -= 180;
+        clicked = false;
+    }
+}
 
 let menubarToggleBtnClicked = false;
 menubarToggleBtn.onclick = function(){
